098e26486437e1798e675d900dafd4e2ed3c899f
Aryan Raj <aryanxrajj@gmail.com>
2025-07-04 23:48:00 +0530

    Kotlin Fundamentals

--- src/ClassesAndObjects/Overriding.kt
+++ src/ClassesAndObjects/Overriding.kt
@@ -1,2 +1,49 @@
 package ClassesAndObjects
 
+open class Gadgets(val name : String , val category : String){
+    var deviceStatus = "On"
+
+    open fun turnOn(){
+
+    }
+
+    open fun turnOff(){
+
+    }
+
+    open fun increaseBrightNessLevel(){
+
+    }
+}
+
+class Laptop(val deviceName : String, val deviceCategory : String) :
+Gadgets(name = deviceName , category = deviceCategory){
+
+    var brightnessLevel = 0
+        set(value) {
+            if(value in 1 .. 100){
+                field = value
+            }
+        }
+
+    override fun increaseBrightNessLevel(){
+        brightnessLevel++
+        println("BrightNess Level Increase by ${brightnessLevel}")
+    }
+
+    override fun turnOn(){
+        deviceStatus = "On"
+        println("BrighNessLevel ${brightnessLevel}")
+    }
+
+    override fun turnOff(){
+        deviceStatus = "Off"
+        println("Device is turnedOff")
+    }
+}
+
+fun main(){
+    var gadget : Gadgets = Laptop("MacBook","Productivity")
+    gadget.turnOn()
+    gadget.increaseBrightNessLevel()
+}
